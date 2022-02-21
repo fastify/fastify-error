85e3b93371c35139f2871c028b9abe65ddfb39fd
SindreXie <xtx1130@gmail.com>
2022-02-21 20:27:13 +0800

    feat: change statement from function to class

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { inherits, format } = require('util')
+const { format } = require('util')
 
 function createError (code, message, statusCode = 500, Base = Error) {
   if (!code) throw new Error('Fastify error code must not be empty')
@@ -8,34 +8,38 @@ function createError (code, message, statusCode = 500, Base = Error) {
 
   code = code.toUpperCase()
 
-  function FastifyError (a, b, c) {
-    if (!(this instanceof FastifyError)) {
-      return new FastifyError(a, b, c)
+  class FastifyError extends Base {
+    constructor (a, b, c) {
+      super()
+      if (!new.target) {
+        return new FastifyError(a, b, c)
+      }
+      Error.captureStackTrace(this, FastifyError)
+      this.name = 'FastifyError'
+      this.code = code
+
+      // more performant than spread (...) operator
+      if (a && b && c) {
+        this.message = format(message, a, b, c)
+      } else if (a && b) {
+        this.message = format(message, a, b)
+      } else if (a) {
+        this.message = format(message, a)
+      } else {
+        this.message = message
+      }
+
+      this.statusCode = statusCode || undefined
     }
-    Error.captureStackTrace(this, FastifyError)
-    this.name = 'FastifyError'
-    this.code = code
 
-    // more performant than spread (...) operator
-    if (a && b && c) {
-      this.message = format(message, a, b, c)
-    } else if (a && b) {
-      this.message = format(message, a, b)
-    } else if (a) {
-      this.message = format(message, a)
-    } else {
-      this.message = message
+    get [Symbol.toStringTag] () {
+      return 'Error'
     }
 
-    this.statusCode = statusCode || undefined
+    toString () {
+      return `${this.name} [${this.code}]: ${this.message}`
+    }
   }
-  FastifyError.prototype[Symbol.toStringTag] = 'Error'
-
-  FastifyError.prototype.toString = function () {
-    return `${this.name} [${this.code}]: ${this.message}`
-  }
-
-  inherits(FastifyError, Base)
 
   return FastifyError
 }
