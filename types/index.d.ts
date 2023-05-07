07335d552b9a88538b2b6bb1ea7021b0996e876b
Trim21 <trim21.me@gmail.com>
2023-05-08 01:48:09 +0800

    prettier: semi false

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,11 +5,11 @@ declare function createError<C extends string, SC extends number, Arg extends un
   Base?: Error
 ): createError.FastifyErrorConstructor<
   {
-    code: C;
-    statusCode: SC;
+    code: C
+    statusCode: SC
   },
   Arg
->;
+>
 
 declare function createError<C extends string, Arg extends unknown[] = [any?, any?, any?]>(
   code: C,
@@ -18,10 +18,10 @@ declare function createError<C extends string, Arg extends unknown[] = [any?, an
   Base?: Error
 ): createError.FastifyErrorConstructor<
   {
-    code: C;
+    code: C
   },
   Arg
->;
+>
 
 declare function createError<Arg extends unknown[] = [any?, any?, any?]>(
   code: string,
@@ -30,34 +30,34 @@ declare function createError<Arg extends unknown[] = [any?, any?, any?]>(
   Base?: Error
 ): createError.FastifyErrorConstructor<
   {
-    code: string;
+    code: string
   },
   Arg
->;
+>
 
-type CreateError = typeof createError;
+type CreateError = typeof createError
 
 declare namespace createError {
   export interface FastifyError extends Error {
-    code: string;
-    name: string;
-    statusCode?: number;
+    code: string
+    name: string
+    statusCode?: number
   }
 
   export interface FastifyErrorConstructor<
     E extends { code: string; statusCode?: number } = {
-      code: string;
-      statusCode?: number;
+      code: string
+      statusCode?: number
     },
     T extends unknown[] = [any?, any?, any?]
   > {
-    new (...arg: T): FastifyError & E;
-    (...arg: T): FastifyError & E;
-    readonly prototype: FastifyError & E;
+    new (...arg: T): FastifyError & E
+    (...arg: T): FastifyError & E
+    readonly prototype: FastifyError & E
   }
 
-  export const createError: CreateError;
-  export { createError as default };
+  export const createError: CreateError
+  export { createError as default }
 }
 
-export = createError;
+export = createError
