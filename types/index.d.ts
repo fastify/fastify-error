fe77f842ca38e34c5833a16a075037f820cebbea
Trim21 <trim21.me@gmail.com>
2023-05-08 14:46:51 +0800

    fix: standard in typescript (#99)

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,19 +1,19 @@
-declare function createError<C extends string, SC extends number>(code: C, message: string, statusCode: SC, Base?: Error): createError.FastifyErrorConstructor<{ code: C; statusCode: SC }>;
-declare function createError<C extends string>(code: C, message: string, statusCode?: number, Base?: Error): createError.FastifyErrorConstructor<{ code: C; }>;
+declare function createError<C extends string, SC extends number> (code: C, message: string, statusCode: SC, Base?: Error): createError.FastifyErrorConstructor<{ code: C, statusCode: SC }>
+declare function createError<C extends string> (code: C, message: string, statusCode?: number, Base?: Error): createError.FastifyErrorConstructor<{ code: C }>
 
-type CreateError = typeof createError;
+type CreateError = typeof createError
 
 declare namespace createError {
   export interface FastifyError extends Error {
-    code: string;
-    name: string;
-    statusCode?: number;
+    code: string
+    name: string
+    statusCode?: number
   }
 
-  export interface FastifyErrorConstructor<E extends { code: string; statusCode?: number } = { code: string; statusCode?: number }> {
-    new (a?: any, b?: any, c?: any): FastifyError & E;
-    (a?: any, b?: any, c?: any): FastifyError & E;
-    readonly prototype: FastifyError & E;
+  export interface FastifyErrorConstructor<E extends { code: string, statusCode?: number } = { code: string, statusCode?: number }> {
+    new (a?: any, b?: any, c?: any): FastifyError & E
+    (a?: any, b?: any, c?: any): FastifyError & E
+    readonly prototype: FastifyError & E
   }
 
   export const createError: CreateError
