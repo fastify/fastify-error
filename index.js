246c427c13f1fdf07ee63adf44735a15411cd409
uzlopak <aras.abbasi@googlemail.com>
2023-05-09 10:21:55 +0200

    fix merge error

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function createError (code, message, statusCode = 500, Base = Error) {
 
   function FastifyError () {
     if (!new.target) {
-      return new FastifyError(...args)
+      return new FastifyError(...arguments)
     }
     this.code = code
     this.name = 'FastifyError'
