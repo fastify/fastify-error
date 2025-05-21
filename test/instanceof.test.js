6bf37420fa172befa4a3f6bccab9788ef656bd50
Aras Abbasi <aras.abbasi@googlemail.com>
2025-05-21 17:30:15 +0200

    simplify

--- test/instanceof.test.js
+++ test/instanceof.test.js
@@ -88,7 +88,7 @@ test('ensure that instanceof works accross different installations of the fastif
     const { foo } = require('dep')
 
     function normalizePath (path) {
-      const normalizedPath = path.slice(path.indexOf("fastify-error-instanceof-test-") - 1)
+      const normalizedPath = path.slice(path.indexOf("fastify-error-instanceof-test-"))
       return normalizedPath
     }
 
@@ -135,7 +135,7 @@ test('ensure that instanceof works accross different installations of the fastif
     const { createError } = require('fastify-error')
     
     function normalizePath (path) {
-      const normalizedPath = path.slice(path.indexOf("fastify-error-instanceof-test-") - (process.platform === 'win32' ? 2 : 1))
+      const normalizedPath = path.slice(path.indexOf("fastify-error-instanceof-test-"))
       return normalizedPath
     }
 
